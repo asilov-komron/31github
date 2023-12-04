ed9be5e3d06e21d7ba433b1fe1600f96ed59e873
asilov-komron <113261257+asilov-komron@users.noreply.github.com>
2023-12-04 09:26:04 +0100

    Create Solution.java

---
+++ Plus One/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+       
+        
+        for(int i = digits.length - 1; i >= 0; i-- ){
+            if(digits[i] == 9){
+                digits[i] = 0;
+                continue;
+
+            }
+
+            digits[i]++;
+            return digits;
+
+        }
+        
+        if(digits[0] == 0){
+            int[] new_arr = new int[digits.length + 1];
+            new_arr[0] = 1;
+            for(int i = 1; i < new_arr.length; i++){
+                new_arr[i] = digits[i-1];
+            }
+
+            return new_arr;
+        }
+
+
+
+
+
+    }
+}
