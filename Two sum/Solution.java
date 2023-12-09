ad98811cdd44041f60011b648be6f7e1d49a6c40
asilov-komron <113261257+asilov-komron@users.noreply.github.com>
2023-12-09 08:19:10 +0100

    Create Solution.java

---
+++ Two sum/Solution.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> dict = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(dict.containsKey(complement)){
+                return new int[] {dict.get(complement), i};
+            }
+            dict.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("Not found");
+    }
+}
