93aa26f7e70b44bdab5ed1ffffeddaa284ae27df
asilov-komron <113261257+asilov-komron@users.noreply.github.com>
2023-12-26 09:04:44 +0100

    Create Solution.java

---
+++ Integer to Roman/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", 
+                        "L","XL", "X", "IX", "V", "IV", "I"};
+        int i = 0;
+
+        while(num > 0){
+            if(num >= values[i]){
+                num = num - values[i];
+                sb.append(romans[i]);
+            }
+            else{
+                i++;
+            }
+        }
+        return sb.toString();
+        
+    }
+}
